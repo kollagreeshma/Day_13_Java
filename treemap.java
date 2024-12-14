b9e51c3f78706eaeea3c78e387a5790dd27451a5
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-14 14:36:23 +0530

    Add files via upload

---
+++ treemap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class treemap
+{
+  public static void main(String args[])
+  {
+    TreeMap<String,String> a=new TreeMap<>();
+    a.put("sno","01");
+    a.put("sname","muraja");
+    System.out.println(a);
+  }
+}
